0c581490189dd2a31da561f3580da6cc81f1bc83
rk <ramakrishna8051@gmail.com>
2019-05-29 19:07:24 -0700

    queries with select, insert, update

--- autograder_mysql/source/queries.js
+++ autograder_mysql/source/queries.js
@@ -3,9 +3,15 @@ module.exports = {
         query: 'Select count(*) from user'
     },
     query2: {
-        query: "insert into user values (1,'test1',23);"
+        query: "insert into user(ID,NAME,AGE) values (1,'test1',23), (2,'test2',24)"
     },
     query3: {
+        query: "update user set NAME = 'test1a' where ID = 1"
+    },
+    query4: {
+        query: "update user set NAME = 'test1a' where ID = 1"
+    },
+    query5: {
         query: 'Select count(*) from user'
     }
 }
--- autograder_mysql/source/validate.js
+++ autograder_mysql/source/validate.js
@@ -5,11 +5,21 @@ module.exports = {
         desc: 'return the number of users'
     },
     query2: {
-        query: 'Select count(*) from user where NAME = "test1"',
+        query: 'Select count(*) from user',
         score: 1,
-        desc: 'return inserted user'
+        desc: 'return inserted count'
     },
     query3: {
+        query: 'Select count(*) from user where NAME = "test1a"',
+        score: 1,
+        desc: 'return update count'
+    },
+    query4: {
+        query: 'Select count(*) from user',
+        score: 1,
+        desc: 'return update count'
+    },
+    query5: {
         query: 'Select count(*) from user',
         score: 1,
         desc: 'return all users'
